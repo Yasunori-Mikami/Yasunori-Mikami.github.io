0065fc56cbab3994ee4872abde17a62c0e665ee8
Yasunori Mikami <hapihapihappylife021@gmail.com>
2018-08-17 10:05:56 +0900

    MOD

--- G's_kadai/02/js/bm_script.js
+++ G's_kadai/02/js/bm_script.js
@@ -27,39 +27,16 @@ $('#categoryAdd').on('click',function(){
     $('.categoryInput').val('');
 });
 
-// //クリックされたら・・
-// $( '.categoryInput' ).keypress( function ( e ) {
-//     if ( e.which == 13 ) {
 
-//         //iの回数を保存したい
-//         //ローカルストレージに i を保存
-//         localStorage.setItem('i_count', i);
-
-//         //テキストエリアに入力されたデータ
-//         let v_category = $('.categoryInput').val();
-
-//         //ローカルストレージに v を保存
-//         localStorage.setItem('category' + i, v_category);
-
-//         // カテゴリ追加
-//         let category_item =  '<li id="category' + '_' + i + ' " ' + ' ' + 'class="category_item">' + v_category + '</li>' ;
-//         $('.tab').append(category_item);
-
-//         //再度非表示にする
-//         $('.categoryInput').removeClass('open');
-
-//         // --------------コンテンツ詳細の追加----------------------
-//         $('.mainContent').append(bookmark_form);
-
-//         return false;
-//     }
-// });
 
 
 let v_category; //テキストエリアに入力されたデータ
 // let categoryObject;//カテゴリの配列宣言
 let categoryNo;//カテゴリID
 let allObject = {};//ここに続けてbookmarkの配列をネストで入れていく
+
+//メインカラム
+let ulNo;  //カテゴリに紐付いて追加されるulのID
 
 //クリックされたら(配列Ver)・・
 $( '.categoryInput' ).keypress( function ( e ) {
@@ -92,32 +69,10 @@ $( '.categoryInput' ).keypress( function ( e ) {
         //ーーーーーーーーフォームのoptionboxにカテゴリ名の追加ーーー
         $('#categorySelect').append('<option value = category' + i+'>' + allObject.categoryObject.categoryName + '</option>');
 
+
         return false;
     }
 });
-
-//------------ロード時に表示----------------
-//---カテゴリの数値----
-let get_i = parseInt(localStorage.getItem('i_count'),10); //10進法でカウント
-if(get_i){
-    for (v = 1; v <= get_i; v++) {
-        if(localStorage.getItem('category' + v)){
-            let v_categoryLoad = JSON.parse(localStorage.getItem('category' + v));//JSONからの変換
-
-            //console.log(v_category.categoryName);
-            //v_category.categoryNameでキーの値を拾ってくる
-            $('.tab').append('<li id="category' + '_' + v + ' " ' + ' ' + 'class="category_item">' + v_categoryLoad.categoryName + '</li>');
-            // $('#category' + v).val(v_category);
-
-
-            //formのoptionにカテゴリ名を表示
-            $('#categorySelect').append('<option value = category' + v+'>' + v_categoryLoad.categoryName + '</option>');
-
-        }
-    }
-}
-var i = $('.category_item').length; //カテゴリの数を数えて初期値に入れる
-
 
 
 /*----------ブックマークフォーム--------*/
@@ -126,6 +81,7 @@ let selectVal;
 let titleVal;
 let urlVal;
 let memoVal;
+let bookmark; //bookmarkの出力HTML
 
 $('#bookmarkAdd').on('click',function(){
     console.log('open');
@@ -159,11 +115,77 @@ $('#registerBtn').on('click',function(){
     };
 
     //ローカルに保存
-    if(selectVal == categoryNo){
-    };
-    localStorage.setItem(categoryNo, JSON.stringify(allObject));
-
+    localStorage.setItem(selectVal, JSON.stringify(allObject));//selectVal = categoryNo なのでそのまま入れる
+    // console.log('selectVal =' + ' ' + selectVal);
+    // console.log('categoryNo =' + ' ' + categoryNo);
 
     alert('ブックマークの登録が完了しました');
     $('#bookmarkform_modal').removeClass('open');
+
+
+    //----------------mainカラムに ul 追加--------------
+    ulNo = 'ulNo' + i; //ulのID
+    $('.mainContent').append('<ul class="main_detail" id=' + ulNo + '></ul>');
+
+    //メインカラムにブックマークアイテムを追加していく
+    let bookmark = '<li><div class="picBox"><img src="img/firet.png" alt=""><p class="registerDay">2017/06/28</p></div><div class="textInfo"><p class="bkmTitle">'+ titleVal +'</p><p class="bkmUrl"><a href="'+ urlVal +'" target="_blank">' + urlVal + '</a></p><p class="bkmMemo">' + memoVal + '</p></div></li>';
+    console.log(bookmark);
+
+    $('#' + ulNo).append(bookmark);
+
 });
+
+
+
+//------------ロード時に表示--------------------------------------
+//---カテゴリの数値----
+let get_i = parseInt(localStorage.getItem('i_count'),10); //10進法でカウント
+if(get_i){
+    for (v = 1; v <= get_i; v++) {
+        if(localStorage.getItem('category' + v)){
+            let v_categoryLoad = JSON.parse(localStorage.getItem('category' + v));//JSONからの変換
+
+
+            //-----------サイドバーの挙動----------------
+            //---配列無し----
+            //console.log(v_category.categoryName);
+            //v_category.categoryNameでキーの値を拾ってくる
+            $('.tab').append('<li id="category' + '_' + v + ' " ' + ' ' + 'class="category_item">' + v_categoryLoad.categoryName + '</li>');
+            // $('#category' + v).val(v_category);
+
+            // //---配列ありの場合-- 調整中箇所
+            // $('.tab').append('<li id="category' + '_' + v + ' " ' + ' ' + 'class="category_item">' + v_categoryLoad.categoryObject.categoryName + '</li>');
+
+
+
+            //formのoptionにカテゴリ名を表示
+            $('#categorySelect').append('<option value = category' + v+'>' + v_categoryLoad.categoryName + '</option>');
+
+            //メインカラムのulの保存
+            ulNo = 'ul' + v;
+            $('.mainContent').append('<ul class="main_detail" id=' + ulNo + '></ul>');
+
+
+            //メインカラムにブックマークアイテムを追加していく
+            let bookmark = '<li><div class="picBox"><img src="img/firet.png" alt=""><p class="registerDay">2017/06/28</p></div><div class="textInfo"><p class="bkmTitle">'+ titleVal +'</p><p class="bkmUrl"><a href="'+ urlVal +'" target="_blank">' + urlVal + '</a></p><p class="bkmMemo">' + memoVal + '</p></div></li>';
+            console.log(bookmark);
+
+            $('#' + ulNo).append(bookmark);
+        }
+    }
+}
+var i = $('.category_item').length; //カテゴリの数を数えて初期値に入れる
+
+
+
+
+//追加でやること
+//（１）リロード時のサイドバー表示の調整（配列にしたので）
+// ✓(2) 配列の保存場所の指定（selectValで）
+// ✓(3) メインカラムに表示させる
+// (４) メインカラムで複数追加表示させる（上書き保存にしない）
+// (5) 非リロード時のタブの挙動
+// (6) 画像のスクレイピング
+// (7) タブの削除、修正機能
+// (8) 保存時間の表示
+// (9) メインカラムのブックマークの削除、修正機能
